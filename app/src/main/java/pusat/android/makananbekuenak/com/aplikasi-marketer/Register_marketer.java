a051deb0024ead3856a8333f46f42dbc77b1aa7a
FidyawatiKamumu <fidyawatikamumu@gmail.com>
2016-03-21 19:46:38 +0800

    hgjk

---
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi-marketer/Register_marketer.java
@@ -0,0 +1,13 @@
+package pusat.android.makananbekuenak.com.aplikasi_marketer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register_marketer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_marketer);
+    }
+}
