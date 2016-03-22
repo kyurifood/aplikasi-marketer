8388685c6896f004057695ee1a3b314b98043c6f
rinaldy95 <rinaldymarasabessy@gmail.com>
2016-03-22 09:10:26 +0800

    updete registrasi marketer

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi-marketer/RegistrasiMarketer.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi-marketer/RegistrasiMarketer.java
@@ -3,11 +3,11 @@ package pusat.android.makananbekuenak.com.aplikasi_marketer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrasiMarketer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registrasi_marketer);
     }
 }
