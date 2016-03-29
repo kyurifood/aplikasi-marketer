c844a725856ca299c5635fd7ad37c58b879713b4
Recanansyah <luffy2460@gmail.com>
2016-03-29 16:18:58 +0700

    Login validasi

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/HomeScreen.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/HomeScreen.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,11 +16,34 @@ public class HomeScreen extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
-<<<<<<< HEAD:app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/HomeScreen.java
 
 
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     //--Pengaturan--
@@ -33,7 +58,6 @@ public class HomeScreen extends AppCompatActivity{
 
         Intent panggil_class = new Intent(this, pusat.android.makananbekuenak.com.aplikasi_marketer.Laporan.class);
         startActivity(panggil_class);
-=======
         Button pengaturan = (Button) findViewById(R.id.pengaturan);
         Button pesananan = (Button) findViewById(R.id.pesanan);
 
@@ -51,7 +75,6 @@ public class HomeScreen extends AppCompatActivity{
             }
         });
 
->>>>>>> ca7cd99725aa269d82d601a27e801c34301e8e84:app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/HomeScreen.java
     }
 
 }
