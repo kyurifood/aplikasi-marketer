4bb96edb383ddfa3ce5be9b4cadcb193ff70196a
SatriaTolinggilo <stolinggilo@gmail.com>
2016-04-20 17:34:55 +0800

    qq

---
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/TampilanPribadi.java
@@ -0,0 +1,13 @@
+package pusat.android.makananbekuenak.com.aplikasi_marketer;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TampilanPribadi extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tampilan_data_pribadi);
+    }
+}
