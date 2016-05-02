f1df1226a46d7313e00189ce511cb8cc6ae4025b
rinaldy95 <rinaldymarasabessy@gmail.com>
2016-05-02 16:00:04 +0800

    lihat detail

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/adapter/List_item_pesanan.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/adapter/List_item_pesanan.java
@@ -1,6 +1,8 @@
 package pusat.android.makananbekuenak.com.aplikasi_marketer.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import pusat.android.makananbekuenak.com.aplikasi_marketer.Lihatdetail;
 import pusat.android.makananbekuenak.com.aplikasi_marketer.MainActivity;
 import pusat.android.makananbekuenak.com.aplikasi_marketer.R;
 import pusat.android.makananbekuenak.com.aplikasi_marketer.domain.Item_pesanan;
@@ -21,6 +24,10 @@ import pusat.android.makananbekuenak.com.aplikasi_marketer.domain.Item_pesanan;
  * Created by opaw on 3/30/16.
  */
 public class List_item_pesanan extends BaseAdapter {
+
+    String var_nama, var_tanggal, var_kode, var_bank, var_nominal, var_regional, var_distributor, var_marketer,
+            var_costomer, var_nohp, var_alamatpenerima, var_produk, var_ongkir, var_pajak;
+
     public Context context;
     private List<Item_pesanan> items;
 
@@ -51,11 +58,11 @@ public class List_item_pesanan extends BaseAdapter {
 
         final Item_pesanan item = items.get(position);
 
-        TextView no_order = (TextView) convertView.findViewById(R.id.no_order);
-        TextView tanggal_pesan = (TextView) convertView.findViewById(R.id.tgl_pesan);
-        TextView nama = (TextView) convertView.findViewById(R.id.nama);
-        TextView bank = (TextView) convertView.findViewById(R.id.bank);
-        TextView nominal = (TextView) convertView.findViewById(R.id.nominal);
+        final TextView no_order = (TextView) convertView.findViewById(R.id.no_order);
+        final TextView tanggal_pesan = (TextView) convertView.findViewById(R.id.tgl_pesan);
+        final TextView nama = (TextView) convertView.findViewById(R.id.nama);
+        final TextView bank = (TextView) convertView.findViewById(R.id.bank);
+        final TextView nominal = (TextView) convertView.findViewById(R.id.nominal);
 
 
         Button btnAction1 = (Button) convertView.findViewById(R.id.btn_action_1);
@@ -73,6 +80,48 @@ public class List_item_pesanan extends BaseAdapter {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, item.getNo_order(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnAction1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                var_nama = nama.getText().toString();
+                var_bank = bank.getText().toString();
+                var_tanggal = tanggal_pesan.getText().toString();
+                var_kode = no_order.getText().toString();
+                var_nominal = nominal.getText().toString();
+                var_regional = String.valueOf(item.getRegional());
+                var_distributor = String.valueOf(item.getDistributor());
+                var_marketer = String.valueOf(item.getMarketer());
+                var_costomer = String.valueOf(item.getCustomer());
+                var_nohp = String.valueOf(item.getNo_hp_penerima());
+                var_alamatpenerima = String.valueOf(item.getAlamat_penerima());
+                var_produk = String.valueOf(item.getProduk());
+                var_ongkir = String.valueOf(item.getOngkir());
+                var_pajak = String.valueOf(item.getPajak());
+
+
+                Intent i = null;
+                i = new Intent(context, Lihatdetail.class);
+                Bundle b = new Bundle();
+
+                b.putString("panggil_nama", var_nama);
+                b.putString("panggil_bank", var_bank);
+                b.putString("panggil_tanggal", var_tanggal);
+                b.putString("panggil_kode", var_kode);
+                b.putString("panggil_nominal", var_nominal);
+                b.putString("panggil_regional", var_regional);
+                b.putString("panggil_distributor", var_distributor);
+                b.putString("panggil_marketer", var_marketer);
+                b.putString("panggil_customer", var_costomer);
+                b.putString("panggil_nohp", var_nohp);
+                b.putString("panggil_alamat", var_alamatpenerima);
+                b.putString("panggil_produk", var_produk);
+                b.putString("panggil_ongkir", var_ongkir);
+                b.putString("panggil_pajak", var_pajak);
+                i.putExtras(b);
+                context.startActivity(i);
             }
         });
 
