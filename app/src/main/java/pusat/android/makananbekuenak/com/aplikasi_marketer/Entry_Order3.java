7b7d5921e418f6f2e6031d75ce94dd04119fd267
etonhusin23 <etonhusin23@gmail.com>
2016-05-04 15:44:41 +0700

    entry order screen 3

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/Entry_Order3.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_marketer/Entry_Order3.java
@@ -11,34 +11,9 @@ public class Entry_Order3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.entry_order_3);
-        Button edit_belanja = (Button) findViewById(R.id.btn_edit_belanja);
-        Button edit_pembeli = (Button) findViewById(R.id.btn_edit_pembeli);
+        setContentView(R.layout.entry_order_3_3);
 
         getSupportActionBar().setTitle("Entri Order");
-
-        Button proses = (Button) findViewById(R.id.btn_proses);
-
-        edit_belanja.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Entry_Order.class);
-                startActivity(intent);
-            }
-        });
-
-        edit_pembeli.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Entry_Order2.class);
-                startActivity(intent);
-            }
-        });
-
-        proses.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(Entry_Order3.this, "Data Berhasil",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
 
     }
 
